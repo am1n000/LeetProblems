808f9f56722d5ac3f33f9f7e25d411f7e278996d
am1n000 <86247014+am1n000@users.noreply.github.com>
2022-09-30 17:24:38 +0100

    Time: 74 ms (26.27%), Space: 71.5 MB (12.39%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -0,0 +1,38 @@
+// struct ListNode
+// {
+//   int val;
+//   ListNode *next;
+//   ListNode() : val(0), next(nullptr) {}
+//   ListNode(int x) : val(x), next(nullptr) {}
+//   ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        ListNode *result = new ListNode;
+        ListNode *ptr = result;
+        div_t sum = {0, 0};
+        while (sum.quot == 1 || l1 || l2)
+        {
+            if (l1)
+            {
+                sum.quot += l1->val; 
+                l1 = l1->next;
+            }
+            if (l2)
+            {
+                sum.quot += l2->val; 
+                l2 = l2->next;
+            }
+            sum = div(sum.quot, 10);
+            result->next = new ListNode(sum.rem);
+            result = result->next;
+        }
+        result = ptr->next;
+        return (result);
+    }
+};
